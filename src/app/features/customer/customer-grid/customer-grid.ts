742856f30b243716d150ed350db00c4afc1bf759
Allen Carvallo <crvlhallu@gmail.com>
2026-03-21 12:33:51 +0530

    Toast message service added

--- src/app/features/customer/customer-grid/customer-grid.ts
+++ src/app/features/customer/customer-grid/customer-grid.ts
@@ -11,6 +11,7 @@ import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { MatDialog } from '@angular/material/dialog';
 import { CustomerForm } from '../customer-form/customer-form';
+import { ToastService } from '../../../shared/services/toast.service';
 
 @Component({
   selector: 'app-customer-grid',
@@ -35,6 +36,7 @@ export class CustomerGrid {
   private customerService = inject(CustomerService);
   private appState = inject(AppState);
   private dialog = inject(MatDialog);
+  private toast = inject(ToastService);
 
   customerList = new MatTableDataSource<CustomerResponse>([]);
   customerCols: string[] = ['name', 'phone', 'email', 'description', 'actions'];
@@ -71,6 +73,7 @@ export class CustomerGrid {
 
     dialogRef.afterClosed().subscribe(() => {
       this.getCustomerList();
+      this.toast.success('Customer added successfully');
     });
   }
 
